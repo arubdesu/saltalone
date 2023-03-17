2dbd9ebaf1022cfc3b6e4d9b8bab21317d014991
Allister Banks <arubdesu@me.com>
2023-03-17 13:49:37 +0900

    python3-ify
    chicken, meet egg - now needing xcodeCLItools 🤪

--- pre.sh
+++ pre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-AUTOPKG_LATEST=$(curl https://api.github.com/repos/autopkg/autopkg/releases | python -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["assets"][0]["browser_download_url"]')
+AUTOPKG_LATEST=$(curl https://api.github.com/repos/autopkg/autopkg/releases | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj[0]["assets"][0]["browser_download_url"])')
 curl -L "${AUTOPKG_LATEST}" -o /tmp/apkg.pkg
 sudo installer -pkg /tmp/apkg.pkg -target /
 autopkg repo-add mosen-recipes && autopkg run -vvv --key PYVERSION=3 salt.install
