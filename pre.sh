48816d4c374029e6ef002027915c7c2162ae914c
Allister Banks <arubdesu@me.com>
2023-12-20 16:25:12 +0900

    reflect broken salt recipe
    gotta fetch manually but hey, there's an ARM version now yay

--- pre.sh
+++ pre.sh
@@ -5,8 +5,10 @@ set -x
 AUTOPKG_LATEST=$(curl https://api.github.com/repos/autopkg/autopkg/releases | /usr/bin/python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj[0]["assets"][0]["browser_download_url"])')
 curl -L "${AUTOPKG_LATEST}" -o /tmp/apkg.pkg
 sudo installer -pkg /tmp/apkg.pkg -target /
-autopkg repo-add mosen-recipes && autopkg run -vvv --key PYVERSION=3 salt.install
-sudo echo "master_type: disable" >> /etc/salt/minion
-sudo echo "log_level: debug" >> /etc/salt/minion
-sudo echo "enable_fqdns_grains: False" >> /etc/salt/minion
-sudo echo "multiprocessing: True" >> /etc/salt/minion
+## as of late 2023 there IS an ARM salt, but autopkg can't find anything py3 'latest'
+## https://repo.saltproject.io/salt/py3/macos/latest/salt-3006.5-py3-arm64.pkg 
+# autopkg repo-add mosen-recipes && autopkg run -vvv --key PYVERSION=3 salt.install
+# sudo echo "master_type: disable" >> /etc/salt/minion
+# sudo echo "log_level: debug" >> /etc/salt/minion
+# sudo echo "enable_fqdns_grains: False" >> /etc/salt/minion
+# sudo echo "multiprocessing: True" >> /etc/salt/minion
