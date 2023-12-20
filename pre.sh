6814d78a32af1bc30ba899ed77330a8e131fcc17
Allister Banks <arubdesu@me.com>
2023-12-20 16:14:45 +0900

    py3-ify
    can't print without parens durrr

--- pre.sh
+++ pre.sh
@@ -2,7 +2,7 @@
 # gotta also have git, via xcodeclitools which'll give us a python3 as well
 # https://gist.github.com/arubdesu/c180ebd0b0aefadd23cf88197343d7fc
 set -x
-AUTOPKG_LATEST=$(curl https://api.github.com/repos/autopkg/autopkg/releases | /usr/bin/python3 -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["assets"][0]["browser_download_url"]')
+AUTOPKG_LATEST=$(curl https://api.github.com/repos/autopkg/autopkg/releases | /usr/bin/python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj[0]["assets"][0]["browser_download_url"])')
 curl -L "${AUTOPKG_LATEST}" -o /tmp/apkg.pkg
 sudo installer -pkg /tmp/apkg.pkg -target /
 autopkg repo-add mosen-recipes && autopkg run -vvv --key PYVERSION=3 salt.install
